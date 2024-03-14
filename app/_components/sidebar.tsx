f93e48604afd26a703dfabb2d9ccb245ebba2271
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-03-14 23:30:42 +0900

    prettierとesLint, tailwindのコード整形プラグインアンインストール

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,16 +1,15 @@
-"use client";
+'use client';
 
-import Profile from "./profile";
-import Title from "./title";
-import Navigation from "./navigation";
-import ThemeChanger from "./(themes)/theme-switch";
-import SNS from "./sns";
+import Profile from './profile';
+import Title from './title';
+import Navigation from './navigation';
+import ThemeChanger from './(themes)/theme-switch';
+import SNS from './sns';
 
 const SideBar = () => {
   return (
-    <div className="hidden lg:sticky lg:top-0 lg:left-0 lg:py-5 lg:h-svh lg:w-full lg:grid lg:grid-flow-row lg:col-span-2">
-      <div className="text-center h-[48px]">
-        {" "}
+    <div className='hidden lg:sticky lg:top-0 lg:left-0 lg:py-5 lg:h-svh lg:w-full lg:grid lg:grid-flow-row lg:col-span-2'>
+      <div className='text-center h-[48px]'>
         {/* This `text-center` for Title for Sidebar */}
         <Title isInSide={false} /> {/* falseでtext-center */}
       </div>
@@ -20,10 +19,10 @@ const SideBar = () => {
       <div>
         <Navigation />
       </div>
-      <div className="flex items-end justify-center pb-4">
+      <div className='flex items-end justify-center pb-4'>
         <ThemeChanger />
       </div>
-      <div className="flex items-start justify-center">
+      <div className='flex items-start justify-center'>
         <SNS />
       </div>
     </div>
