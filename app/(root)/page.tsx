aee508f640b0493b3c76b77d3e207cc8ed630783
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-04-01 01:14:23 +0900

    topページのレイアウト変更

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,21 +7,19 @@ import Title from '../_components/title';
 export default async function Home() {
   const limitedBlogs = await getData();
 
+  const text = 'Hello World!!';
+  console.log(text.at(0));
+
   return (
-    <main className='max-w-xl mx-auto lg:max-w-full lg:col-span-4 xl:col-span-5 w-full h-auto lg:flex lg:flex-col lg:justify-between'>
-      <div className='w-auto h-auto mt-5 ml-7'>
-        <Title isInSide /> {/* trueで no text-center */}
-      </div>
-      <div className='w-full h-auto mt-16 xl:mt-0'>
-        <div className='xl:gap-2 lg:gap-y-20 lg:grid lg:grid-cols-1 xl:grid-cols-2 xl:grid-rows-2 w-full h-full'>
+    <main className='w-full h-full max-w-xl mx-auto lg:max-w-full lg:col-span-4 xl:col-span-5 lg:flex lg:flex-col lg:justify-between'>
+      <div className='w-full h-full mt-16 xl:mt-0'>
+        <div className='w-full h-full flex flex-col xl:flex-row xl:items-center'>
           <TopPageSection>
             <Skills />
           </TopPageSection>
           <TopPageSection>
             <Blog blogs={limitedBlogs} />
           </TopPageSection>
-          <TopPageSection></TopPageSection>
-          <TopPageSection></TopPageSection>
         </div>
       </div>
     </main>
